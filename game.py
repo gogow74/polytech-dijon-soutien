26987d6b0934de23c4d8747d7059ca07bbe83761
gogow <hugo.viard73@gmail.com>
2026-09-14 16:27:37 +0200

    saisie d'une combinaison de couleurs

--- game.py
+++ game.py
@@ -11,7 +11,23 @@ def code_creation():
     return code
 
 def afficher_couleurs():
-
     for i in range(len(config.colors)):
         print(config.colors[i][0], ":", config.colors[i][1])
 
+def saisie_code():
+    lettres_valides = []
+    for i in range(len(config.colors)):
+        lettres_valides.append(config.colors[i][0])
+
+    saisie = []
+    for i in range(config.code_length):
+        saisieInvalide = True
+        while saisieInvalide:
+            #enelve les espaces et met tt en maj
+            x = input(f"Choix de la couleur {i+1} : ").strip().upper()
+            if x in lettres_valides :
+                saisieInvalide = False
+            else:
+                print("Saisie invalide")
+        saisie.append(x)
+    return saisie
